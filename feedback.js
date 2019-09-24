9f37ec82e931fa81c3192022084445dc771c6983
Tore <torealex@ntnu.no>
2019-09-24 22:09:12 +0200

    added feedback class and testing different styles, not finished...

---
+++ feedback.js
@@ -0,0 +1,63 @@
+/**
+ * Class for showing feedback
+ * eid: DOM element id where the feedback should live *REQUIRED*
+ * takes inn params (objec)
+ * pos: position where the feedback div is placed ('t', 'b', 'l', 'r')
+ *      default: 't'
+ * dismissable: Boolean wheter feedbacks are dismissable
+ *              default: true
+ *
+ * This class comes with a style sheet (feedback.css)
+ */
+const pos = ['t', 'b', 'l', 'r']
+
+class FeedBack {
+	constructor(eid, params = {}) {
+		if (!eid) {
+			console.error('An element ID is required')
+			return void 0
+		}
+		this.eid = eid
+		let defaultParams = {
+			pos: 't',
+			dismissable: true
+		}
+		if (params.pos && !pos.includes(params.pos))
+			console.error(
+				'position must be on of the following: ',
+				pos.join(', '),
+				'\ncontinuing with default position!'
+			)
+		this.params = { ...defaultParams, ...params }
+		window.onloadend = this.init()
+	}
+	init() {
+		this.feedback = document.createElement('div')
+		this.feedback.classList.add('feedback')
+
+		if (this.params.pos == 't')
+			document.getElementById(this.eid).prepend(this.feedback)
+		else if (this.params.pos == 'b')
+			document.getElementById(this.eid).append(this.feedback)
+	}
+
+	push(msg, cls = null) {
+		let btn
+		let fb = document.createElement('div')
+		fb.innerHTML = msg
+		fb.classList.add('feedback', 'content')
+		if (cls) fb.classList.add(cls)
+		if (this.params.dismissable) {
+			// add dismiss button
+			btn = document.createElement('button')
+			btn.classList.add('dismiss')
+			btn.innerHTML = 'X'
+			fb.append(btn)
+			console.log(btn)
+			btn.onclick = function() {
+				console.log(this.getParent)
+			}
+		}
+		this.feedback.append(fb)
+	}
+}
