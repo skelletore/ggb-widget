8c25a3115ecf4098533fe548d6d2fa7643ae1c63
skelletore <forbregd.tore@gmail.com>
2019-10-07 13:16:35 +0200

    Added more complicated conditions...

--- feedback.js
+++ feedback.js
@@ -9,121 +9,121 @@
  *
  * This class comes with a style sheet (feedback.css)
  */
-const pos = ['t', 'b', 'l', 'r']
+const pos = ["t", "b", "l", "r"]
 
 class FeedBack {
-	constructor(
-		eid,
-		params = {},
-		conditions = [],
-		fbs = [],
-		def = null,
-		vars = null,
-		ans = null
-	) {
-		if (!eid) {
-			console.error('An element ID is required')
-			return void 0
-		}
-		this.eid = eid
-		let defaultParams = {
-			dismissable: true,
-			multi: true,
-			forgetful: false,
-			random: true
-		}
+  constructor(
+    eid,
+    params = {},
+    conditions = [],
+    fbs = [],
+    def = null,
+    vars = null,
+    ans = null
+  ) {
+    if (!eid) {
+      console.error("An element ID is required")
+      return void 0
+    }
+    this.eid = eid
+    let defaultParams = {
+      dismissable: true,
+      multi: true,
+      forgetful: false,
+      random: true
+    }
 
-		this.params = { ...defaultParams, ...params }
-		this.conditions = conditions
-		this.feedbacks = fbs
-		this.defaultFb = def
-		this.givenFb = []
-		this.vars = vars
-		this.feedbackCB = ans
-		window.onloadend = this.init()
-	}
-	init() {
-		this.container = document.createElement('div')
-		this.container.classList.add('feedback-container')
-		this.feedback = document.createElement('div')
-		this.feedback.classList.add('feedback')
-		let heading = document.createElement('div')
-		heading.classList.add('feedback-heading')
-		heading.innerHTML = '<em>FEEDBACK</em>'
-		let footer = document.createElement('div')
-		footer.classList.add('feedback-footer')
-		let btn = document.createElement('div')
-		btn.classList.add('feedback-button')
-		btn.innerHTML = 'CHECK'
-		btn.onclick = this.checkAns
-		footer.append(btn)
-		this.container.append(heading, this.feedback, footer)
+    this.params = { ...defaultParams, ...params }
+    this.conditions = conditions
+    this.feedbacks = fbs
+    this.defaultFb = def
+    this.givenFb = []
+    this.vars = vars
+    this.feedbackCB = ans
+    window.onloadend = this.init()
+  }
+  init() {
+    this.container = document.createElement("div")
+    this.container.classList.add("feedback-container")
+    this.feedback = document.createElement("div")
+    this.feedback.classList.add("feedback")
+    let heading = document.createElement("div")
+    heading.classList.add("feedback-heading")
+    heading.innerHTML = "<em>FEEDBACK</em>"
+    let footer = document.createElement("div")
+    footer.classList.add("feedback-footer")
+    let btn = document.createElement("div")
+    btn.classList.add("feedback-button")
+    btn.innerHTML = "CHECK"
+    btn.onclick = this.checkAns
+    footer.append(btn)
+    this.container.append(heading, this.feedback, footer)
 
-		document.getElementById(this.eid).append(this.container)
-		// 	document.getElementById(this.eid).classList.add('feedback-column')
-		// else document.getElementById(this.eid).classList.add('feedback-row')
-		document.getElementById(this.eid).classList.add('feedback-grid')
-	}
-	checkAns = event => {
-		let conds = []
-		for (let c of this.conditions) {
-			let ch = check(c, this.vars)
-			conds.push(ch)
-			// console.log(`checking condition ${c}, evaluated to ${ch}`)
-		}
-		let fbs = this.feedbacks
-			.filter((f, i) => {
-				return conds[i]
-			})
-			.flat()
-			.filter(f => !this.givenFb.includes(f))
-		if (!fbs.length && !this.givenFb.includes(this.defaultFb)) {
-			this.givenFb.push(this.defaultFb)
-			this.push(this.defaultFb, 'error')
-			return
-		} else if (fbs.length) {
-			// console.log(fbs)
-			let rand = Math.random()
-			let floor = Math.floor
-			let fb = fbs[floor(rand * fbs.length + 1) - 1]
-			this.givenFb.push(fb)
-			this.push(fb)
-			return
-		}
-		event.disabled = true
-	}
+    document.getElementById(this.eid).append(this.container)
+    // 	document.getElementById(this.eid).classList.add('feedback-column')
+    // else document.getElementById(this.eid).classList.add('feedback-row')
+    document.getElementById(this.eid).classList.add("feedback-grid")
+  }
+  checkAns = event => {
+    let conds = []
+    for (let c of this.conditions) {
+      let ch = check(c, this.vars)
+      conds.push(ch)
+      // console.log(`checking condition ${c}, evaluated to ${ch}`)
+    }
+    let fbs = this.feedbacks
+      .filter((f, i) => {
+        return conds[i]
+      })
+      .flat()
+      .filter(f => !this.givenFb.includes(f))
+    if (!fbs.length && !this.givenFb.includes(this.defaultFb)) {
+      this.givenFb.push(this.defaultFb)
+      this.push(this.defaultFb, "error")
+      return
+    } else if (fbs.length) {
+      // console.log(fbs)
+      let rand = Math.random()
+      let floor = Math.floor
+      let fb = fbs[floor(rand * fbs.length + 1) - 1]
+      this.givenFb.push(fb)
+      this.push(fb)
+      return
+    }
+    event.disabled = true
+  }
 
-	push(msg, cls = 'standard') {
-		let btn
-		let fbWrapper = document.createElement('div')
-		fbWrapper.classList.add('content-wrapper')
-		let fb = document.createElement('div')
-		fb.classList.add('content')
-		let status = document.createElement('span')
-		status.classList.add('status', cls)
-		let fbmsg = document.createElement('span')
-		fbmsg.classList.add('feedback-message')
-		fbmsg.innerHTML = msg
-		fb.append(status, fbmsg)
-		fbWrapper.append(fb)
-		// if (cls) fb.classList.add(cls)
-		if (this.params.dismissable) {
-			// add dismiss button
-			btn = document.createElement('button')
-			btn.classList.add('dismiss')
-			btn.innerHTML = 'X'
-			fb.append(btn)
-			console.log(btn)
-			btn.onclick = function(event) {
-				event.target.parentElement.style.display = 'none'
-			}
-		}
-		if (!this.params.multi) {
-			while (this.feedback.firstChild) {
-				this.feedback.removeChild(this.feedback.firstChild)
-			}
-		}
-		this.feedback.prepend(fbWrapper)
-		this.feedbackCB(msg)
-	}
+  push(msg, cls = "info") {
+    let btn
+    let fbWrapper = document.createElement("div")
+    fbWrapper.classList.add("content-wrapper")
+    let fb = document.createElement("div")
+    fb.classList.add("content")
+    let status = document.createElement("span")
+    status.classList.add("status", cls)
+    let fbmsg = document.createElement("span")
+    fbmsg.classList.add("feedback-message")
+    fbmsg.innerHTML = msg
+    fb.append(status, fbmsg)
+    fbWrapper.append(fb)
+    // if (cls) fb.classList.add(cls)
+    if (this.params.dismissable) {
+      // add dismiss button
+      btn = document.createElement("button")
+      btn.classList.add("dismiss")
+      btn.innerHTML = "X"
+      fb.append(btn)
+      console.log(btn)
+      btn.onclick = function(event) {
+        event.target.parentElement.style.display = "none"
+      }
+    }
+    if (!this.params.multi) {
+      while (this.feedback.firstChild) {
+        this.feedback.removeChild(this.feedback.firstChild)
+      }
+    }
+    this.feedback.prepend(fbWrapper)
+    this.feedbackCB(msg)
+  }
 }
