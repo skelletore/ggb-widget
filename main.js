68352cc0bfd4ca6cbeca827ac6757b8c6ef8493a
Tore <torealex@ntnu.no>
2019-10-08 22:33:40 +0200

    Added clientListener, interesting hidden feature of GGB Also added possibility to select different configs in the browser, doesnt work properly as the global ggbApplet object doesnt unregister...

--- main.js
+++ main.js
@@ -1,41 +1,58 @@
-import Widget from "./ggbWidget.js"
+import Widget from './ggbWidget.js'
 // import Widget from './test.js'
 
-const configFile = "configA.json"
+const configFile = 'configA.json'
+// const configFile = 'configB.json'
 
-/**
- * Widget constructor
- * Widget (divElement, config, answer, onAnswer, options)
- */
-let answerEl = document.getElementById("answer")
+const divContainer = document.getElementById('widget-container')
+const fileUpload = document.getElementById('config-file')
+
+fileUpload.onchange = inn => {
+	let file = fileUpload.files[0]
+	let fr = new FileReader()
+	fr.onload = evt => {
+		let config = JSON.parse(evt.target.result)
+		makeWidget(config)
+	}
+	fr.readAsText(file)
+}
+let answerEl = document.getElementById('answer')
 let ans
 let onAnswer = answer => {
-  ans = answer
-  // console.log(answer)
-  // let answerJSON = JSON.stringify(answer, null, 2)
-  // answerEl.innerText = answerJSON
-  answerEl.value = answer
+	answerEl.value = answer
 }
-let divElement = document.getElementById("widget")
 
 fetch(`./configs/${configFile}`)
-  .then(resp => resp.json())
-  .then(
-    config =>
-      (window.widget = new Widget(divElement.id, config, null, onAnswer, {}))
-  )
+	.then(resp => resp.json())
+	.then(config => makeWidget(config))
 
-let setAns = () => {
-  console.log("setting answer...")
-  let ansWidget = new Widget(
-    document.getElementById("setAns").id,
-    config,
-    ans,
-    function(arg) {
-      console.log("new ans:", arg)
-    },
-    {}
-  )
+function makeWidget(config) {
+	// let divEl = document.getElementById('widget')
+	if (divContainer.hasChildNodes())
+		divContainer.removeChild(divContainer.firstChild)
+	answerEl.value = ''
+	delete window.widget
+	let divEl = document.createElement('div')
+	divEl.id =
+		'widget' +
+		Math.random()
+			.toString(36)
+			.substring(2, 15)
+	divContainer.append(divEl)
+	window.widget = new Widget(divEl.id, config, null, onAnswer, {})
 }
 
-document.getElementById("setBtn").onclick = setAns
+let setAns = () => {
+	console.log('setting answer...')
+	let ansWidget = new Widget(
+		document.getElementById('setAns').id,
+		config,
+		ans,
+		function(arg) {
+			console.log('new ans:', arg)
+		},
+		{}
+	)
+}
+
+document.getElementById('setBtn').onclick = setAns
